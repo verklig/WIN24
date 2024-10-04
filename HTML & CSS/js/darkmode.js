6509667bcbe6fe55dd6079b1e2a67c9a1175cb2b
verklig <eric.blohm@live.se>
2024-10-04 12:48:43 +0200

    adding finished darkmode

---
+++ HTML & CSS/js/darkmode.js
@@ -0,0 +1,34 @@
+const darkmodeSwitch = document.querySelector('#darkmode-switch')
+const hasDarkmode = localStorage.getItem('darkmode')
+
+if(hasDarkmode == null) {
+  if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    enableDarkMode()
+  } else {
+    disableDarkMode()
+  }
+} else if(hasDarkmode === 'on') {
+  enableDarkMode()
+} else if(hasDarkmode === 'off') {
+  disableDarkMode()
+}
+
+darkmodeSwitch.addEventListener('change', () => {
+  if(darkmodeSwitch.checked) {
+    enableDarkMode()
+    localStorage.setItem('darkmode', 'on')
+  } else {
+    disableDarkMode()
+    localStorage.setItem('darkmode', 'off')
+  }
+})
+
+function enableDarkMode() {
+  darkmodeSwitch.checked = true
+  document.documentElement.classList.add('dark')
+}
+
+function disableDarkMode() {
+  darkmodeSwitch.checked = false
+  document.documentElement.classList.remove('dark')
+}
